c7a3a2240d9d6f85891f52e7bb68ea6690c10049
marlins0 <97695970+marlins0@users.noreply.github.com>
2022-04-03 23:40:52 -0600

    trying again im gonna cry

--- week11.js
+++ week11.js
@@ -1,29 +1,17 @@
 
 function buttonClicked () {
-  document.getElementById('button-1').style.background='#000000';//.classList.click('.is-red');
+  document.getElementById('button-1').classList.click('.is-red');
 
-// }
+}
 
-// function getsBigger() {
-    // document.getElementById('button-2').classList.mouseover('.gets-bigger');
-// }
+function getsBigger() {
+    document.getElementById('button-2').classList.mouseover('.gets-bigger');
+ }
 
-// function windowLoaded() {
-  // document.getElementById('button-1').addEventListener('click', buttonClicked);
-  // document.getElementById('button-2').addEventListener('mouseover', buttonHovered);
+ function windowLoaded() {
+   document.getElementById('button-1').addEventListener('click', buttonClicked);
+   document.getElementById('button-2').addEventListener('mouseover', buttonHovered);
 
-// }
+}
 
-// window.onload = windowLoaded;
-const btn = document.getElementById('btn');
-
-let index = 0;
-
-const colors = ['salmon', 'green', 'blue', 'purple'];
-
-btn.addEventListener('click', function onClick() {
-  btn.style.backgroundColor = colors[index];
-  btn.style.color = 'white';
-
-  index = index >= colors.length - 1 ? 0 : index + 1;
-});
+ window.onload = windowLoaded;
