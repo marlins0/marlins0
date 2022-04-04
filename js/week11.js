b78705743e61c90eefa4586b732c29f8c4263b37
marlins0 <97695970+marlins0@users.noreply.github.com>
2022-04-03 23:07:28 -0600

    tried an internet thing

--- js/week11.js
+++ js/week11.js
@@ -1,17 +1,29 @@
 
-function buttonClicked () {
-  document.getElementById('button-1').classList.click('.is-red');
+// function buttonClicked () {
+  // document.getElementById('button-1').classList.click('.is-red');
 
-}
+// }
 
 // function getsBigger() {
     // document.getElementById('button-2').classList.mouseover('.gets-bigger');
 // }
 
-function windowLoaded() {
-  document.getElementById('button-1').addEventListener('click', buttonClicked);
+// function windowLoaded() {
+  // document.getElementById('button-1').addEventListener('click', buttonClicked);
   // document.getElementById('button-2').addEventListener('mouseover', buttonHovered);
 
-}
+// }
 
-window.onload = windowLoaded;
+// window.onload = windowLoaded;
+const btn = document.getElementById('btn');
+
+let index = 0;
+
+const colors = ['salmon', 'green', 'blue', 'purple'];
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[index];
+  btn.style.color = 'white';
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
