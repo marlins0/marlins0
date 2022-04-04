4ba3201676ff2d018f83104283534ae4d45d2c43
marlins0 <97695970+marlins0@users.noreply.github.com>
2022-04-03 19:49:24 -0600

    took out button 2

--- js/week11.js
+++ js/week11.js
@@ -4,13 +4,13 @@ function buttonClicked () {
 
 }
 
-function getsBigger() {
-    document.getElementById('button-2').classList.mouseover('.gets-bigger');
-}
+// function getsBigger() {
+    // document.getElementById('button-2').classList.mouseover('.gets-bigger');
+// }
 
 function windowLoaded() {
   document.getElementById('button-1').addEventListener('click', buttonClicked);
-  document.getElementById('button-2').addEventListener('mouseover', buttonHovered);
+  // document.getElementById('button-2').addEventListener('mouseover', buttonHovered);
 
 }
 
