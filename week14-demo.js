86480e9189931b84e9164c787fbcc3fe91cd0f2f
marlins0 <97695970+marlins0@users.noreply.github.com>
2022-04-21 11:00:53 -0600

    okay

---
+++ week14-demo.js
@@ -0,0 +1,70 @@
+const myCar = new Object();
+myCar.color = 'Gray';
+myCar.make = 'Mazda';
+myCar.model = 'CX-5';
+myCar.year = '2017';
+
+
+// general syntac " object.property = value;"
+
+
+console.log(myCar.make);
+
+// another way to do it
+  const mySecondCar = {
+    color: 'Blue',
+    make: 'Ford',
+    model: 'Ranger',
+    year: '2020'
+  }
+
+  console.log(mySecondCar.make);
+  console.log(myCar['make']);
+///////
+
+
+    const superHeroes = {
+  “squadName”: “Super hero squad”,
+  “homeTown”: “Metro City”,
+  “formed”: 2016,
+  “secretBase”: “Super tower”,
+  “active”: true,
+  “members”: [
+    {
+      // an array of object object object
+      //superHeros['members'][0]; to get Molecule man
+      “name”: “Molecule Man”,
+      “age”: 29,
+      “secretIdentity”: “Dan Jukes”,
+      “powers”: [
+        “Radiation resistance”,
+        “Turning tiny”,
+        “Radiation blast”
+      ]
+    },
+    {
+      “name”: “Madame Uppercut”,
+      “age”: 39,
+      “secretIdentity”: “Jane Wilson”,
+      “powers”: [
+        “Million tonne punch”,
+        “Damage resistance”,
+        “Superhuman reflexes”
+      ]
+    },
+    {
+      “name”: “Eternal Flame”,
+      “age”: 1000000,
+      “secretIdentity”: “Unknown”,
+      “powers”: [
+        “Immortality”,
+        “Heat Immunity”,
+        “Inferno”,
+        “Teleportation”,
+        “Interdimensional travel”
+      ]
+    }
+  ]
+}
+
+console.log(superHeros['members'][0]);
